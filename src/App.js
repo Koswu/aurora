b41a90e327c654a2e29ab73978b24f857a6deeb5
koswu <ws00298046@163.com>
2021-04-19 17:19:27 +0800

    增加首页图标

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import React, {useState} from 'react';
 import { BrowserRouter, Route, Switch, Link, Redirect } from 'react-router-dom';
 import ReactCanvasNest from 'react-canvas-nest';
 import HeaderNav from './common/ui/header';
-import { Container, Modal } from 'react-bootstrap';
+import { Card, CardDeck, Container, Modal } from 'react-bootstrap';
 import {testServer} from './api/testconn.js'
 import storageKeys from './common/variable/storagekey.js'
 import NodeSearch from './view/nodesearch.js'
@@ -14,9 +14,14 @@ import InfoNodeAdd from './view/nodeadd.js'
 import SourceNodeAdd from './view/sourceadd.js'
 import GraphView from './view/showgraph.js'
 import ShowNodeView from './view/shownode.js'
-import Footer from './common/ui/footer.js'
 import SourceNodeSearch from './view/sourcesearch';
 import ShowSourceNodeView from './view/showsource';
+import {InfoIcon} from '@primer/octicons-react';
+import SearchInfoIcon from './icons/laptop_search.svg'
+import AddInfoIcon from './icons/add_doc.svg'
+import AddSourceIcon from './icons/add_site.svg'
+import SearchSourceIcon from './icons/site_search.svg'
+import RelationIcon from './icons/relation.svg'
 
 class Index extends React.Component {
   constructor(props) {
@@ -25,16 +30,53 @@ class Index extends React.Component {
   render() {
     return (
       <div className="center-area">
-        <Link to={'/infonode/add'} ><Button>增加信息节点</Button></Link>
-        <Link to={'/infonode/search'}><Button>信息节点检索</Button></Link>
-        <Link to={'/sourcenode/add'}><Button>添加外部来源</Button></Link>
-        <Link to={'/sourcenode/search'}><Button>外部来源节点检索</Button></Link>
-        <Link to={'/graph'}><Button>查看节点关系</Button></Link>
+      <CardDeck>
+        <Link to={'/infonode/add'} >
+          <Card className="main-card text-center">
+            <Card.Img variant="top" src={AddInfoIcon}>
+            </Card.Img>
+            <Card.Body>
+              <Card.Title>增加信息节点</Card.Title>
+            </Card.Body>
+          </Card>
+        </Link>
+        <Link to={'/infonode/search'}>        
+          <Card className="main-card text-center">
+            <Card.Img variant="top" src={SearchInfoIcon}></Card.Img>
+            <Card.Body>
+              <Card.Title>信息节点检索</Card.Title>
+            </Card.Body>
+          </Card>
+        </Link>
+        <Link to={'/sourcenode/add'}>        
+          <Card className="main-card text-center">
+            <Card.Img variant="top" src={AddSourceIcon}></Card.Img>
+            <Card.Body>
+              <Card.Title>添加外部来源</Card.Title>
+            </Card.Body>
+          </Card>
+        </Link>
+        <Link to={'/sourcenode/search'}>
+          <Card className="main-card text-center">
+            <Card.Img variant="top" src={SearchSourceIcon}></Card.Img>
+            <Card.Body>
+              <Card.Title>外部来源检索</Card.Title>
+            </Card.Body>
+          </Card>
+        </Link>
+        <Link to={'/graph'}>
+          <Card className="main-card text-center">
+            <Card.Img variant="top" src={RelationIcon}></Card.Img>
+            <Card.Body>
+              <Card.Title>查看节点关系</Card.Title>
+            </Card.Body>
+          </Card>
+        </Link>
+      </CardDeck>
         <Button onClick={() => testServer((data) => {
           console.log(data);
           alert("OK");
         })}>测试服务器连接</Button>
-        <p>这个是主页</p>
       </div>
     );
   }
--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    APP_NAME: "Aurora",
+    APP_NAME: "智图",
     BACKEND_SERVE_BASEURL: "http://localhost:8080",
 }
 
